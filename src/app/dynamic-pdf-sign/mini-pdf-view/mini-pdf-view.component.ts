e1afcb4f2571c5ec65a7ad68cb89f7bfabcc210e
stevan_radovanovic <stevan.radovanovic@blinking.id>
2021-02-11 15:32:47 +0100

    Enabled download button

--- src/app/dynamic-pdf-sign/mini-pdf-view/mini-pdf-view.component.ts
+++ src/app/dynamic-pdf-sign/mini-pdf-view/mini-pdf-view.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import { MatAnchor } from '@angular/material/button';
 import { MatDialog } from '@angular/material/dialog';
 import { DynamicPdfSignModel } from '../dynamic-pdf-sign.model';
 import { PdfPreviewModalComponent } from '../pdf-preview-modal/pdf-preview-modal.component';
@@ -11,14 +12,25 @@ import { PdfPreviewModalComponent } from '../pdf-preview-modal/pdf-preview-modal
 export class MiniPdfViewComponent implements OnInit {
 
   @Input('miniPdf') miniPdf!: DynamicPdfSignModel;
+  @ViewChild('downloadLink') downloadLink: MatAnchor;
 
   constructor(private dialog: MatDialog) { }
 
   ngOnInit(): void {
   }
 
+  ngAfterViewInit() {
+    console.log(this.downloadLink)
+  }
+
   openPreview() {
     this.dialog.open(PdfPreviewModalComponent,{data: {base64: this.miniPdf.base64}, height: '800px', width: '800px'})
   }
+
+  downloadPdf() {
+    this.downloadLink._elementRef.nativeElement.href = this.miniPdf.base64;
+    this.downloadLink._elementRef.nativeElement.download = `blinking${Date.now().toString()}.pdf`;
+  }
+  
 
 }
